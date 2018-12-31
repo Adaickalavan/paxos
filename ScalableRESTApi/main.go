3dbafcac6e9cf25bd36050a2517e162d60913ec5
Adaickalavan <adaickalavan@gmail.com>
2018-12-31 17:03:33 +0800

    Update

--- ScalableRESTApi/main.go
+++ ScalableRESTApi/main.go
@@ -1,9 +1,55 @@
 package main
 
 import (
-	"fmt"
+	"database"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"time"
 )
 
-func main(){
-	fmt.Print("Hi")
+//Hooks that may be overridden for testing
+var inputReader io.Reader = os.Stdin
+var outputWriter io.Writer = os.Stdout
+
+//Initialize icecream database
+var product = database.Product{}
+
+func main() {
+
+	//Connect to database
+	product.Server = os.Getenv("MONGO_PORT")
+	product.DatabaseName = os.Getenv("DATABASE_NAME")
+	product.CollectionName = os.Getenv("COLLECTION_NAME")
+	product.UserName = os.Getenv("MONGO_USERNAME")
+	product.Password = os.Getenv("MONGO_PASSWORD")
+	product.Session = product.Connect()
+	defer product.Session.Close()
+
+	//Ensure database index is unique
+	product.EnsureIndex([]string{"productID"})
+	product.EnsureIndex([]string{"name"})
+
+	if err := run(); err != nil {
+		log.Fatal(err.Error())
+	}
+}
+
+func run() error {
+	httpAddr := os.Getenv("LISTENING_ADDR")
+	mux := makeMuxRouter()
+	// loggedRouter := handlers.LoggingHandler(outputWriter, mux) //Wrap the mux router to log all api requests. Logged requests are written to `outputWriter`
+	s := &http.Server{
+		Addr:           ":" + httpAddr,
+		Handler:        mux, //To log all api calls, replace `mux` with `loggedRouter` and uncomment `loggedRouter` at 3 lines above
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	log.Println("Listening on ", httpAddr)
+	if err := s.ListenAndServe(); err != nil {
+		return err
+	}
+	return nil
 }
